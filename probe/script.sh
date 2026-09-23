eca5549354e0184e11206efd8ff42f5098f14ee3
Sebastian Dauenhauer <info@magical.rocks>
2026-09-23 18:48:37 +0200

    probe: edition

--- probe/script.sh
+++ probe/script.sh
@@ -1,14 +1,14 @@
 export PATH=/data/rust/bin:/data/usr/bin:/bin
 export HOME=/data/home
 cd /data/src/probe
-rustc -O check.rs -o /tmp/check || exit 3
+rustc --edition 2021 -O check.rs -o /tmp/check || exit 3
 /tmp/check mimic /data/src/probe/mimic.bin
 /tmp/check mimic /tmp/mimic.bin
-rustc --crate-type=rlib -C save-temps --out-dir /data/src/probe lib.rs
+rustc --edition 2021 --crate-type=rlib -C save-temps --out-dir /data/src/probe lib.rs
 echo "probe rlib on btrfs: $?"
 for f in /data/src/probe/*.o; do /tmp/check $f; done
 /tmp/check /data/src/probe/liblib.rlib
-rustc --crate-type=rlib -C save-temps --out-dir /tmp lib.rs
+rustc --edition 2021 --crate-type=rlib -C save-temps --out-dir /tmp lib.rs
 echo "probe rlib on tmpfs: $?"
 for f in /tmp/*.o; do /tmp/check $f; done
 exit 20
